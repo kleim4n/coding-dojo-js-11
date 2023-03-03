0bc76bef9a0cdd83e0a9a5628bfd91d0014fdab6
guilherme nunes <gnlnascimento@gmail.com>
2023-03-03 16:14:31 -0300

    Edição da ul para adição da li

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 function addTask() {
-  const reminderList = document.getElementById('reminder-list')
-  const hour = prompt('Digite a hora escolhida!');
-  const message = prompt("Digite sua mensagem: ")
-  reminderList.appendChild createLi(hour, message);
+	const reminderList = document.getElementById('reminder-list');
+	const hour = prompt('Digite a hora escolhida!');
+	const message = prompt('Digite sua mensagem: ');
+	reminderList.appendChild(createLi(hour, message));
 }
 function createLi(hour, message) {
-  const createItem = document.createElement('li')
-  createItem.innerHTML = `<li class="reminder-item">
+	const createItem = document.createElement('li');
+	createItem.classList.add('reminder-item');
+	createItem.innerHTML = `
   <div>${message}</div>
   <div>${hour}</div>
-  </li>`
-  return createItem
+ `;
+	createItem.classList;
+	return createItem;
 }
