7e6357e29f47925d421bd970c3672199a8095f20
Miguel Zanona Krasner <miguelzkestudos@gmail.com>
2023-03-03 16:42:44 -0300

    Regex e while adicionada ao prompt

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
+const regex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/g;
+
 function addTask() {
 	const reminderList = document.getElementById('reminder-list');
-	const hour = prompt('Digite a hora escolhida!');
+	let hour;
+	while (!regex.test(hour)) {
+		hour = prompt('Digite a hora escolhida! (hh:mm)');
+	}
 	const message = prompt('Digite sua mensagem: ');
 	reminderList.appendChild(createLi(hour, message));
 }
@@ -17,16 +22,18 @@ function createLi(hour, message) {
 
 const date = new Date();
 
-function updateDate(){
-      document.getElementById("display-date").innerHTML = date.toLocaleDateString('pt-BR', { month: 'long', year: 'numeric'});
-    }
-
-function addMonthInDate(){
-  date.setMonth(date.getMonth() +1 );
-  updateDate();
+function updateDate() {
+	document.getElementById("display-date").innerHTML = date.toLocaleDateString('pt-BR', { month: 'short', year: 'numeric' });
 }
 
-function minusMonthInDate(){
-  date.setMonth(date.getMonth() -1 );
-  updateDate();
+updateDate();
+
+function addMonthInDate() {
+	date.setMonth(date.getMonth() + 1);
+	updateDate();
+}
+
+function minusMonthInDate() {
+	date.setMonth(date.getMonth() - 1);
+	updateDate();
 }
